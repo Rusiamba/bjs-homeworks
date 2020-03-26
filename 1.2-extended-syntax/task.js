70553a88e60fea9e77817cb2b0f3e1fe81f8334a
Дмитрий <42304481+Rusiamba@users.noreply.github.com>
2020-03-26 19:01:59 +0300

    1.2
    2 функции

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,36 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let d = b**2 - 4*a*c;
+    if (d < 0) return [];
+    
+    if (d = 0) {
+        let result = -b/2*a;
+        return result;
+    }
+    
+    if (d > 0) {
+        let result1 = (-b + Math.sqrt(d))/2*a;
+        let result2 = (-b - Math.sqrt(d))/2*a;
+        return [result1, result2];
+    }
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    if (marks.length == 0) return 0;
+    if (marks.length > 5) {
+        console.log("Массив больше 5 значений");
+        marks = marks.slice(0, 6);
+    }
+
+    let sum = 0;
+    for (let i = 0; i< marks.length; i++) {
+        sum += marks[i];
+    }
+    return sum/5;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
+    // let name;
+    // let dateOfBirthday;
+
     // return result;
 }
